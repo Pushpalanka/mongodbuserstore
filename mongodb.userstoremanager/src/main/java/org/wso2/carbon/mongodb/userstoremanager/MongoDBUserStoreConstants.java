97e0ec8c325d0fd7ab9e6e362a7a846d08052e56
asantha <asanthatilina@gmail.com>
2016-04-28 11:08:10 -0700

    test done osgi bundle service not started

---
+++ mongodb.userstoremanager/src/main/java/org/wso2/carbon/mongodb/userstoremanager/MongoDBUserStoreConstants.java
@@ -0,0 +1,38 @@
+package org.wso2.carbon.mongodb.userstoremanager;
+
+import java.util.ArrayList;
+
+import org.wso2.carbon.user.api.Property;
+
+public class MongoDBUserStoreConstants {
+
+	 public static final ArrayList<Property> CUSTOM_UM_MANDATORY_PROPERTIES = new ArrayList<Property>();
+	    public static final ArrayList<Property> CUSTOM_UM_OPTIONAL_PROPERTIES = new ArrayList<Property>();
+	    public static final ArrayList<Property> CUSTOM_UM_ADVANCED_PROPERTIES = new ArrayList<Property>();
+
+
+	    private static void setProperty(String name, String displayName, String value, String description) {
+	        Property property = new Property(name, value, displayName + "#" +description, (Property[])null);
+	        CUSTOM_UM_OPTIONAL_PROPERTIES.add(property);
+
+	    }
+
+	    private static void setMandatoryProperty(String name, String displayName, String value, String description) {
+	        Property property = new Property(name, value, displayName + "#" +description, (Property[])null);
+	        CUSTOM_UM_MANDATORY_PROPERTIES.add(property);
+
+	    }
+
+	    private static void setAdvancedProperty(String name, String displayName, String value, String description) {
+	        Property property = new Property(name, value, displayName + "#" +description, (Property[])null);
+	        CUSTOM_UM_ADVANCED_PROPERTIES.add(property);
+
+	    }
+
+	    static {
+	        setMandatoryProperty("ServiceURL", "Service URL", "http://", "location of webservice");
+	        setMandatoryProperty("ServicePort", "Service Port",  "27017", "Service Port which monogodb connect to");
+	        setProperty("Username", "User Name",  "root", "User Name to connect to mongodb (if provided)");
+	        setProperty("Password", "Enable Password", "", "Password to connect to mongodb server (if provided any)");
+	    }
+}
---
+++ mongodb.userstoremanager/src/main/java/org/wso2/carbon/mongodb/userstoremanager/internal/MongoDBUserStoreDSComponent.java
@@ -0,0 +1,40 @@
+package org.wso2.carbon.mongodb.userstoremanager.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.mongodb.userstoremanager.MongoDBUserStoreManager;
+import org.wso2.carbon.user.api.UserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * @scr.component name="mongodb.userstoremanager.dscomponent" immediate=true
+ */
+
+public class MongoDBUserStoreDSComponent {
+
+	private static Log log = (Log) LogFactory.getLog(MongoDBUserStoreDSComponent.class);
+   // private static RealmService realmService;
+
+    protected void activate(ComponentContext ctxt) {
+
+        MongoDBUserStoreManager userStoreManager = new MongoDBUserStoreManager();
+        ctxt.getBundleContext().registerService(UserStoreManager.class.getName(), userStoreManager, null);
+        log.info("MongoDB User Store bundle activated successfully..");
+        System.out.println("Mongo Started");
+    }
+
+    protected void deactivate(ComponentContext ctxt) {
+        if (log.isDebugEnabled()) {
+            log.debug("MongoDB User Store Manager is deactivated ");
+        }
+    }
+
+    /*protected void setRealmService(RealmService rlmService) {
+          realmService = rlmService;
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+        realmService = null;
+    }*/
+}
