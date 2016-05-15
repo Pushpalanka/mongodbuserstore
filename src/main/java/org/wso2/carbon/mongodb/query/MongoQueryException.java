a30ccd2660b41593384895afb2dcd47d65ab1bf4
asanthamax <asanthathilina@gmail.com>
2016-05-15 06:41:58 -0400

    Correct some issues in MongoDBUserStore Manager Class

---
+++ src/main/java/org/wso2/carbon/mongodb/query/MongoQueryException.java
@@ -0,0 +1,27 @@
+package org.wso2.carbon.mongodb.query;
+
+public class MongoQueryException extends Exception{
+
+	private static final long serialVersionUID = 1997753363232807009L;
+
+	public MongoQueryException(){
+		
+	}
+	
+	public MongoQueryException(String message){
+		
+		super(message);
+	}
+	
+	public MongoQueryException(Throwable reason){
+		
+		super(reason);
+	}
+	
+	public MongoQueryException(String message,Throwable reason){
+		
+		super(message,reason);
+	}
+	
+	
+}
